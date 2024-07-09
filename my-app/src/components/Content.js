ded4da191a0de27359e7953a63ce2ad0f2205ff7
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-09 16:06:45 +0200

    feat: Add Loader component and integrate loading logic in Content component

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,19 +2,31 @@ import React, { Component } from 'react'
 import css from './css/Content.module.css'
 import { savedPosts } from '../posts.json'
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 export default class Content extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {}
+    this.state = {
+      isLoaded: false,
+    }
   }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        isLoaded: true,
+      })
+    }, 2000)
+  }
+
   render() {
     return (
       <div>
         <div className={css.TitleBar}></div>
         <div className={css.Searchresults}>
-          <PostItem savedPosts={savedPosts} />
+          {this.state.isLoaded ? <PostItem savedPosts={savedPosts} /> : <Loader />}
         </div>
       </div>
     )
